92c0fd9263695bcb7522b7d69e1071d0c9c997af
Lena Efremova <miranskayaelena@gmail.com>
2017-11-26 12:05:21 +0300

    fixed tests description

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 describe('Array', function() {
-  describe('#indexOf()', function() {
+  describe('#indexOf()first', function() {
     it('should return -1 when the value is not present', function() {
       var a = [1,2,3].indexOf(4);
       console.log(a);
@@ -8,28 +8,28 @@ describe('Array', function() {
 
     });
   });
-  describe('#indexOf(22)', function() {
-    it('-1 when the value is not present', function() {
-      var a = [1,2,3].indexOf(4);
+  describe('#indexOf()second', function() {
+    it('should return 2 when the value is present', function() {
+      var a = [1,2,3].indexOf(3);
       console.log(a);
-      assert.equal(-1, a);
+      assert.equal(2, a);
 
     });
   });
 });
 
-describe('Aay', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      var a = [1,2,3].indexOf(4);
+describe('Array2', function() {
+  describe('#indexOf()first', function() {
+    it('should return 3 when the value is present', function() {
+      var a = [1,2,3,4,5].indexOf(4);
       console.log(a);
-      assert.equal(-1, a);
+      assert.equal(3, a);
 
     });
   });
-  describe('#indexOf(22)', function() {
+  describe('#indexOf()second', function() {
     it('-1 when the value is not present', function() {
-      var a = [1,2,3].indexOf(4);
+      var a = [1,2,3,4,5].indexOf(47);
       console.log(a);
       assert.equal(-1, a);
 
