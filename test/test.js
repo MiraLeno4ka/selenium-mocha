6f9729976486aec8cfcb67229ca676cd63bf92dc
Lena Efremova <miranskayaelena@gmail.com>
2017-11-26 11:28:40 +0300

    selenium chrome example, mocha example

---
+++ test/test.js
@@ -0,0 +1,38 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      var a = [1,2,3].indexOf(4);
+      console.log(a);
+      assert.equal(-1, a);
+
+    });
+  });
+  describe('#indexOf(22)', function() {
+    it('-1 when the value is not present', function() {
+      var a = [1,2,3].indexOf(4);
+      console.log(a);
+      assert.equal(-1, a);
+
+    });
+  });
+});
+
+describe('Aay', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      var a = [1,2,3].indexOf(4);
+      console.log(a);
+      assert.equal(-1, a);
+
+    });
+  });
+  describe('#indexOf(22)', function() {
+    it('-1 when the value is not present', function() {
+      var a = [1,2,3].indexOf(4);
+      console.log(a);
+      assert.equal(-1, a);
+
+    });
+  });
+});
